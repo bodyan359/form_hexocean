9c204f18da9385ca92d6980b2afe86262369dca7
Bohdan Yukhymchuk <yukhymchb@gmail.com>
2021-06-01 22:37:54 +0200

    Add redux-form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import { Field } from "redux-form";
 
 function Form() {
-  return <div className="Form">2</div>;
+  return (
+    <div className="Form">
+      <form>
+        <div>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" component="input" type="text" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <Field name="email" component="input" type="email" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
